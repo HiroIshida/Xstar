93b44940bc4bd6481c5b381766669f8798469523
HiroIshida <spitfire.docomo@gmail.com>
2021-05-28 02:14:16 +0900

    Tweak example

--- example.jl
+++ example.jl
@@ -1,6 +1,7 @@
 using Revise
 using Xstar
 using Plots
+using Random
 
 using StaticArrays
 using LinearAlgebra
@@ -11,15 +12,16 @@ function Xstar.is_obstacle_free(rrtstar::RRTStar, x)
     return norm(x[1:2] - [0.5, 0.5]) > 0.3
 end
 
+Random.seed!(1)
 
 cspace = BoxSpace(3, [0.0, 0, -2*π], [1., 1., 2*π])
 x_start = SVector{3, Float64}([0.1, 0.1, 0.0])
 x_goal = SVector{3, Float64}([0.9, 0.9, 0.0])
-rrtstar = RRTStar(cspace, x_start, x_goal; metric=ReedsSheppMetric(0.2), mu=0.3) 
-@time for i in 1:3000
+rrtstar = RRTStar(cspace, x_start, x_goal; metric=ReedsSheppMetric(0.1), mu=0.3) 
+@time for i in 1:1000
     is_success = extend(rrtstar)
     println(i)
-    is_success && break
+    #is_success && break
 end
 
 fig = plot()
