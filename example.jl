56e432b0bf93b6d3ffdfc3f549de37481be2864d
HiroIshida <spitfire.docomo@gmail.com>
2021-05-29 20:45:15 +0900

    Tmp example update

--- example.jl
+++ example.jl
@@ -14,28 +14,33 @@ end
 
 Random.seed!(1)
 
-function example_dubins_car()
+#function example_reedsshepp()
     cspace = BoxSpace(3, [0.0, 0, -2*π], [1., 1., 2*π])
     x_start = SVector{3, Float64}([0.1, 0.1, 0.0])
-    x_goal = SVector{3, Float64}([0.9, 0.9, 0.0])
-    rrtstar = RRTStar(cspace, x_start, x_goal; metric=PythonReedsSheppMetric(0.1), mu=0.3) 
+    x_goal = SVector{3, Float64}([0.5, 0.9, 0.0])
+    rrtstar = RRTStar(cspace, x_start, x_goal; metric=CppReedsSheppMetric(0.2), mu=0.3) 
     @time for i in 1:1000
-        is_success = extend(rrtstar)
         println(i)
+        is_success = extend(rrtstar)
+        if mod(i, 10000)==1
+            fig = plot()
+            visualize!(rrtstar, fig, with_solution=true)
+            savefig(fig, "figs/plot_"*string(i)*".png")
+        end
         #is_success && break
     end
 
     fig = plot()
-    visualize!(rrtstar, fig, with_solution=false)
+    visualize!(rrtstar, fig, with_solution=true)
+#end
 
-    # plot circle
-    function circle(h, k, r)
-        θ = LinRange(0, 2*π, 500)
-        h .+ r*sin.(θ), k .+ r*cos.(θ)
-    end
-    plot!(fig, circle(0.5, 0.5, 0.3), label="", seriestype=[:shape],
-          c=:blue, linecolor=:black, lw=0.5, fillalpha=0.2)
+# plot circle
+function circle(h, k, r)
+    θ = LinRange(0, 2*π, 500)
+    h .+ r*sin.(θ), k .+ r*cos.(θ)
 end
+plot!(fig, circle(0.5, 0.5, 0.3), label="", seriestype=[:shape],
+      c=:blue, linecolor=:black, lw=0.5, fillalpha=0.2)
 
 function example_euclidean()
     dim = 3
@@ -60,5 +65,3 @@ function example_euclidean()
     plot!(fig, circle(0.5, 0.5, 0.3), label="", seriestype=[:shape],
           c=:blue, linecolor=:black, lw=0.5, fillalpha=0.2)
 end
-
-example_dubins_car()
--- src/Xstar.jl
+++ src/Xstar.jl
@@ -37,6 +37,36 @@ function uniform_sampling(box::BoxSpace)
     r = box.lo + width .* rand(Float64, box.dim)
 end
 
+struct EllipticSE2 <: ConfigurationSpace{3}
+    x_center::SVector{2, Float64} # TODO Euclid 2
+    r1::Float64
+    r2::Float64
+    angle::Float64
+end
+function EllipticSE2(c_best, x_start, x_goal)
+    c_min = norm(x_start[1:2] - x_goal[1:2])
+    r1 = c_best * 0.5
+    r2 = sqrt(c_best^2 - c_min^2) * 0.5
+    angle = atan(x_goal[2] - x_start[2], x_goal[1] - x_start[1])
+    center = 0.5 * (x_start[1:2] + x_goal[1:2])
+    EllipticSE2(center, r1, r2, angle)
+end
+
+function uniform_sampling(cspace::EllipticSE2)
+    # just rejection sampling is find because only 2dim 
+    while true
+        p = 2 * rand(Float64, 2) .* [cspace.r1, cspace.r2] .- [cspace.r1, cspace.r2]
+        is_inside = (p[1]/cspace.r1)^2 + (p[2]/cspace.r2)^2 < 1.0
+        if is_inside
+            a = cspace.angle
+            mat = [cos(a) -sin(a); sin(a) cos(a)]
+            p_ = mat * p + cspace.x_center
+            theta_ = rand() * 2 * π - π
+            return SVector{3, Float64}([p_[1], p_[2], theta_])
+        end
+    end
+end
+
 mutable struct Node{N}
     x::SVector{N, Float64}
     cost::Float64
@@ -111,7 +141,12 @@ function extend(rrtstar::RRTStar{N}) where N
         rrtstar.goal_node.parent_idx = rrtstar.sol_list[idx_best].idx
     end
 
-    x_rand = uniform_sampling(rrtstar.cspace)
+    if solution_found(rrtstar)
+        ellipse = EllipticSE2(rrtstar.goal_node.cost, rrtstar.x_start, rrtstar.x_goal)
+        x_rand = uniform_sampling(ellipse)
+    else
+        x_rand = uniform_sampling(rrtstar.cspace)
+    end
     node_nearest, x_new = _find_nearest_and_new(rrtstar, x_rand)
     is_obstacle_free(rrtstar, node_nearest, x_new) || (return false) 
 
