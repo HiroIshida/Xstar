2d239ae3920a7e892304ba33f8f3b0fc9f43df04
HiroIshida <spitfire.docomo@gmail.com>
2021-07-07 05:39:08 +0900

    Change conditions order

--- src/reeds_shepp_specific.jl
+++ src/reeds_shepp_specific.jl
@@ -1,11 +1,11 @@
 function is_obstacle_free(rrtstar::RRTStar{3, <:ReedsSheppMetric}, node_start::Node, x_target)
+    is_obstacle_free(rrtstar, x_target) || (return false)
     pts = waypoints(rrtstar.metric, node_start.x, x_target, 0.01)
     for pt in pts
         p = SVector{3, Float64}([pt[1], pt[2], pt[3]])
         is_obstacle_free(rrtstar, p) || (return false)
         is_inside(rrtstar.cspace, p) || (return false)
     end
-    is_obstacle_free(rrtstar, x_target) || (return false)
 
     return true
 end
